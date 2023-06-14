ed21103d0d32546130a2f4146d2612a03598eb40
jwc122 <jwc122@ic.ac.uk>
2023-06-14 13:34:50 +0100

    14/06

---
+++ web-app/Game_module.js
@@ -0,0 +1,108 @@
+import R from "../web-app/ramda.js";
+const Game_module = Object.create(null);
+
+
+var board;
+var score = 0;
+var rows = 4;
+var columns = 4;
+export {board, score, rows, columns};
+
+/**
+ * Check if the board is full
+ * @memberof Game_module
+ * @function
+ * @param 
+ * @returns 
+ */
+
+Game_module.setGame = function() {
+    board = [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ];
+    return board;
+};
+
+
+/**
+ * Check if adjacent cells have different values
+ * @memberof Game_module
+ * @function
+ * @param 
+ * @returns 
+ */
+
+/**
+ * Check if the game is over
+ * @memberof Game_module
+ * @function
+ * @param 
+ * @returns 
+ */
+
+/**
+ * Check for empty cells
+ * @memberof Game_module
+ * @function
+ * @param 
+ * @returns {Boolean} 
+ */
+
+Game_module.hasEmptyTile = function(rows, columns) {
+    let count = 0;
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < columns; c++) {
+            if (board[r][c] == 0) { //at least one zero in the board
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+
+
+/**
+ * Move and combine cells
+ * @memberof Game_module
+ * @function
+ * @param 
+ * @returns 
+ */
+
+/**
+ * Insert a new cell where empty
+ * @memberof Game_module
+ * @function
+ * @param 
+ * @returns 
+ */
+
+/**
+ * Calculate the score
+ * @memberof Game_module
+ * @function
+ * @param 
+ * @returns 
+ */
+
+/**
+ * generate random position
+ * @memberof Game_module
+ * @function
+ * @param 
+ * @returns 
+ */
+
+/**
+ * probability of 2 or 4
+ * @memberof Game_module
+ * @function
+ * @param 
+ * @returns 
+ */
+
+export default Object.freeze(Game_module);
